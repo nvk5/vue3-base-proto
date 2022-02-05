12d79e31f3fbdade777cb427fa6dd3cf3e734ac2
nvk5 <nvkdev5@gmail.com>
2022-02-06 00:40:45 +0500

    Update .eslintrc.js
    settings require indent 2 for errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "linebreak-style": 0,
 	"no-tabs": 0,
-	"indent": [4, "tab"]
+	"indent": [2, "tab"]
   },
 };
